a6b7215c529fbb9af53ae95a61d5a683ffce0fd6
Zach Barlow <ztbarlow66@gmail.com>
2020-07-15 22:46:07 -0400

    Updates

---
+++ precache-manifest.575a755a74034af3b633d5d6039e0ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3ee7dbd0e0f6b86d2b9e6cb5375e35",
+    "url": "/shopcart/index.html"
+  },
+  {
+    "revision": "9ce5f75e28d7039440f0",
+    "url": "/shopcart/static/css/main.c1fc59e7.chunk.css"
+  },
+  {
+    "revision": "0cbe421978f34592f003",
+    "url": "/shopcart/static/js/2.db65a22d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopcart/static/js/2.db65a22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce5f75e28d7039440f0",
+    "url": "/shopcart/static/js/main.f9f65678.chunk.js"
+  },
+  {
+    "revision": "a79e419fff915924cd87",
+    "url": "/shopcart/static/js/runtime-main.394ed3d5.js"
+  }
+]);
